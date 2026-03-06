876c8cf369e2867c76893cac41192543cf2836c1
lavkush <lkatiyar12@gmail.com>
2026-03-06 13:04:33 +0530

    adds a route to the homepage of the app
    * send json data on the request for the home page

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
+const users = require("./data.json");
 const app = express();
 const PORT = 8000;
+app.get("/", (request, response) => {
+  console.log(request.body);
+  return response.send(users);
+});
 
-app.listen(PORT,()=> console.log('serverStarted : ',PORT))
+app.listen(PORT, () => console.log("serverStarted : ", PORT));
