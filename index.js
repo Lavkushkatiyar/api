857bad3d25b6a7878dac81d630a74dae1cd3a55e
lavkush <lkatiyar12@gmail.com>
2026-03-06 14:06:38 +0530

    adds a user in the existing data
    * gets post request and adds the user
    * tested api using PostMan

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const users = require("./data.json");
+
+const fs = require("fs");
+
 const app = express();
 const PORT = 8000;
+
 app.get("/", (request, response) => {
   console.log(request.body);
-  return response.send(users);
+  return response.json(users);
+});
+const getHandler = (request, response) => {
+  const id = Number(request.params.id);
+  const user = users.find((user) => user.id === id);
+  return response.json(user);
+};
+
+const patchHandler = (request, response) => {};
+
+const deleteHandler = (request, response) => {};
+
+app.route("/:id").get(getHandler).patch(patchHandler).delete(deleteHandler);
+
+app.use(express.urlencoded({ extended: false }));
+app.post("/", async (request, response) => {
+  const body = request.body;
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./data.json", JSON.stringify(users), (err, res) => {
+    return res.send({ status: "pending" });
+  });
 });
 
 app.listen(PORT, () => console.log("serverStarted : ", PORT));
